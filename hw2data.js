13d2d5704d164a3bfd20e62452db3d3551ae69a1
mario <uswadim@seattleu.edu>
2018-04-30 14:59:37 -0700

    hw files

---
+++ hw2data.js
@@ -0,0 +1,44 @@
+{
+  "data": [
+    {
+      "name": "Robert Downey Jr.",
+      "designation": "President",
+      "salary": "$8,822,000",
+      "joining_date": "2008/05/02",
+      "office": "New York",
+      "extension": "4465"
+    },
+    {
+      "name": "Chris Evans",
+      "designation": "Chief of Executive Officer",
+      "salary": "$5,520,000",
+      "joining_date": "2011/07/22",
+      "office": "Boston",
+      "extension": "6138"
+    },
+    {
+      "name": "Chris Hemsworth",
+      "designation": "Chief of Operating Officer",
+      "salary": "$3,150,000",
+      "joining_date": "2011/05/06",
+      "office": "Melbourne",
+      "extension": "8118"
+    },
+    {
+      "name": "Mark Ruffalo",
+      "designation": "Chief of Data Officer",
+      "salary": "$2,150,000",
+      "joining_date": "2012/05/04",
+      "office": "Kenosha",
+      "extension": "1122"
+    },
+    {
+      "name": "Scarlett Johansson",
+      "designation": "Chief of Strategy Officer",
+      "salary": "$3,825,000",
+      "joining_date": "2010/05/07",
+      "office": "New York",
+      "extension": "1284"
+    }
+  ]
+}
